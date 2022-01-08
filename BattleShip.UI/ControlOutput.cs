6eaf09d579c4f43b0cef5d33a21316733664dc79
ZoranZoltan <msz.kuresevic@gmail.com>
2022-01-08 17:41:27 +0100

    initial commit

---
+++ BattleShip.UI/ControlOutput.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleShip.BLL.GameLogic;
+using BattleShip.BLL.Requests;
+using BattleShip.BLL.Responses;
+using BattleShip.BLL.Ships;
+using System.Threading;
+namespace BattleShip.UI
+{
+    class ControlOutput
+    {
+        static int counttime = 0;
+
+        public static void ShowFlashScreen()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("        *******************************************************************");
+            Console.Write("        *"); Console.ForegroundColor = ConsoleColor.White; Console.Write("    IGRA PODMORNICE!!!   autor: Zoran Zoltan Kuresevic  "); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*");
+            Console.WriteLine("        *******************************************************************");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Timer t = new Timer(ClearFlashScreen, null, 0, 1000);
+            Thread.Sleep(2100); // Simulating other work (10 seconds)
+            t.Dispose(); // Cancel the timer now
+            //Console.ReadKey();
+        }
+
+        private static void ClearFlashScreen(Object state)
+        {
+            if (counttime < 2)
+                counttime += 1;
+            else
+            {
+                Console.Clear();
+                counttime = 0;
+            }
+        }
+
+        public static void ShowHeader()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("        ************************************");
+            Console.Write("        *"); Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("      PODMORNICE!!!     "); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*");
+            Console.WriteLine("        ************************************");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void ShowAllPlayer(Player[] player)
+        {
+            string str = "Igrac 1: " + player[0].Name + "(Pobede: " + player[0].Win + ")\t Igrac 2: " + player[1].Name + "(Pobede: " + player[1].Win + ")";
+            if (player[1].IsPC)
+                str += "\tNivo igre: " + player[1].GameLevel.ToString();
+            Console.WriteLine(str);
+            Console.WriteLine("");
+        }
+
+
+
+
+        public static void ShowWhoseTurn(Player player)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(player.Name + " igra... ");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        static string GetLetterFromNumber(int number)
+        {
+            string result = "";
+            switch (number)
+            {
+                case 1:
+                    result = "A";
+                    break;
+                case 2:
+                    result = "B";
+                    break;
+                case 3:
+                    result = "C";
+                    break;
+                case 4:
+                    result = "D";
+                    break;
+                case 5:
+                    result = "E";
+                    break;
+                case 6:
+                    result = "F";
+                    break;
+                case 7:
+                    result = "G";
+                    break;
+                case 8:
+                    result = "H";
+                    break;
+                case 9:
+                    result = "I";
+                    break;
+                case 10:
+                    result = "J";
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+        public static void DrawHistory(Player player)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Write("  ");
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.Write(y);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(GetLetterFromNumber(x) + " ");
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int y = 1; y <= 10; y++)
+                {
+                    //Console.Write(y);
+                    ShotHistory history = player.PlayerBoard.CheckCoordinate(new Coordinate(x, y));
+                    switch (history)
+                    {
+                        case ShotHistory.Hit:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("H");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case ShotHistory.Miss:
+                            Console.Write("M");
+                            break;
+                        case ShotHistory.Unknown:
+                            Console.Write(" ");
+                            break;
+                    }
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+        }
+
+        public static void ShowShotResult(FireShotResponse shotresponse, Coordinate c, string playername)
+        {
+            String str = "";
+            switch (shotresponse.ShotStatus)
+            {
+                case ShotStatus.Duplicate:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    str = "gadjano polje: " + GetLetterFromNumber(c.XCoordinate) + c.YCoordinate.ToString() + "\t rezultat: to polje je vec gadjano!";
+                    break;
+                case ShotStatus.Hit:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    str = "Gadjano polje: " + GetLetterFromNumber(c.XCoordinate) + c.YCoordinate.ToString() + "\t rezultet: pogodak!";
+                    break;
+                case ShotStatus.HitAndSunk:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    str = "Gadjano polje: " + GetLetterFromNumber(c.XCoordinate) + c.YCoordinate.ToString() + "\t rezultat: pogodak, potopljen je " + shotresponse.ShipImpacted + "!";
+                    break;
+                case ShotStatus.Invalid:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    str = "Gadjano polje: " + GetLetterFromNumber(c.XCoordinate) + c.YCoordinate.ToString() + "\t result: Pogresna lokacija!";
+                    break;
+                case ShotStatus.Miss:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    str = "Gadjano polje: " + GetLetterFromNumber(c.XCoordinate) + c.YCoordinate.ToString() + "\t Rezultat: promasaj!";
+                    break;
+                case ShotStatus.Victory:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    str = "Gadjano polje: " + GetLetterFromNumber(c.XCoordinate) + c.YCoordinate.ToString() + "\t rezultat: pogodak, potopljen je " + shotresponse.ShipImpacted + "! \n\n";
+                    str += "       ******\n";
+                    str += "       ******\n";
+                    str += "        **** \n";
+                    str += "         **  \n";
+                    str += "         **  \n";
+                    str += "       ******\n";
+                    str +="Kraj igre, " + playername + " je pobedio!";                    
+                    break;
+            }
+            Console.WriteLine(str);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+        }
+
+        static void test()
+        {
+            List <object> obj= new List<object>();
+            Player player1 = new Player();
+            Board board1 = new Board();
+            obj.Add(board1);
+            obj.Add(player1);
+        }
+
+        public static void ResetScreen(Player[] player)
+        {
+            Console.Clear();
+            ControlOutput.ShowHeader();
+            ControlOutput.ShowAllPlayer(player);
+        }
+    }
+}
---
+++ BattleShip.UI/GameSetup.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleShip.BLL.GameLogic;
+using BattleShip.BLL.Requests;
+using BattleShip.BLL.Responses;
+using BattleShip.BLL.Ships;
+
+namespace BattleShip.UI
+{
+    public class GameSetup
+    {
+        GameState _gm;
+        public GameSetup(GameState gm)
+        {
+            _gm = gm;
+        }
+
+        public void Setup()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            ControlOutput.ShowFlashScreen();
+            ControlOutput.ShowHeader();
+
+            GameLevel gamelevel = GameLevel.Easy;
+            string[] userSetUp = ControlInput.GetNameFromUser();
+            switch (userSetUp[2])
+            {
+                case "h":
+                    gamelevel = GameLevel.Hard;
+                    break;
+                case "m":
+                    gamelevel = GameLevel.Medium;
+                    break;
+                default:
+                    gamelevel = GameLevel.Easy;
+                    break;
+            }
+
+            _gm.Player1.Name = userSetUp[0];
+            _gm.Player1.IsPC = false;
+            _gm.Player1.Win = 0;
+            _gm.Player1.GameLevel = gamelevel;
+
+            _gm.Player2.Name = userSetUp[1];
+            _gm.Player2.Win = 0;
+            _gm.Player2.GameLevel = gamelevel;
+
+            //vs Computer
+            if (userSetUp[1] == "")
+            {
+                _gm.Player2.Name = "Kompjuter";
+                _gm.Player2.IsPC = true;
+            }
+        }
+
+        public void SetBoard()
+        {
+            ControlOutput.ResetScreen(new Player[] { _gm.Player1, _gm.Player2 });
+
+            _gm.IsPlayer1 = BLL.Responses.GetRandom.WhoseFirst();
+
+            _gm.Player1.PlayerBoard = new Board();
+            PlaceShipOnBoard(_gm.Player1);
+            ControlOutput.ResetScreen(new Player[] { _gm.Player1, _gm.Player2 });
+
+            _gm.Player2.PlayerBoard = new Board();
+            PlaceShipOnBoard(_gm.Player2);
+            Console.WriteLine("Svi brodovi su postavljeni, pritisni bilo koje dugme da nastavis...");
+            Console.ReadKey();
+        }
+
+        public void PlaceShipOnBoard(Player player)
+        {
+            bool IsPlaceBoardAuto = false;
+            if (player.IsPC != true)
+            {
+                ControlOutput.ShowWhoseTurn(player);
+                IsPlaceBoardAuto = ControlInput.IsPlaceBoardAuto();
+                if (!IsPlaceBoardAuto)
+                    Console.WriteLine("Unesi koordinate i tip (l, r, u, d) broda. Ex:) a2, r:");
+            }
+            for (ShipType s = ShipType.Destroyer; s <= ShipType.Carrier; s++)
+            {
+                PlaceShipRequest ShipToPlace = new PlaceShipRequest();
+                ShipPlacement result;
+                do
+                {
+                    if (!player.IsPC && !IsPlaceBoardAuto)
+                    {
+                        ShipToPlace = ControlInput.GetLocationFromUser(s.ToString());
+                        ShipToPlace.ShipType = s;
+                        result = player.PlayerBoard.PlaceShip(ShipToPlace);
+                        if (result == ShipPlacement.NotEnoughSpace)
+                            Console.WriteLine("Nema više mesta!");
+                        else if (result == ShipPlacement.Overlap)
+                            Console.WriteLine("polozaji se preklapaju!");
+                    }
+                    else
+                    {
+                        ShipToPlace = ControlInput.GetLocationFromComputer();
+                        ShipToPlace.ShipType = s;
+                        result = player.PlayerBoard.PlaceShip(ShipToPlace);
+                    }
+
+                } while (result != ShipPlacement.Ok);
+            }
+        }
+    }
+}
